eae564d513f2c6d742b8c98e8a087ef7940fd348
FTC5 <dan.m1512@gmail.com>
2023-12-08 19:50:50 +0200

    Add block UI XRInteractionManager

---
+++ Assets/Scripts/XRInteractionManagerBlockUI.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine.EventSystems;
+using UnityEngine.XR.Interaction.Toolkit;
+
+namespace Assets.Scripts
+{
+    public class XRInteractionManagerBlockUI : XRInteractionManager
+    {
+        protected override void  ProcessInteractors(XRInteractionUpdateOrder.UpdatePhase updatePhase)
+        {
+            if(!EventSystem.current.IsPointerOverGameObject())
+            {
+                Logger.Instance.SendMessage("=_=");
+                base.ProcessInteractors(updatePhase);
+            }
+            
+        }
+    }
+}
