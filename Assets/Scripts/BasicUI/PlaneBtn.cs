2c7ef9be77df55e38766573ef8a22f5dfc9787b9
FTC5 <dan.m1512@gmail.com>
2023-12-24 07:46:44 +0200

    add  toggle plane

---
+++ Assets/Scripts/BasicUI/PlaneBtn.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+namespace Assets.Scripts.BasicUI
+{
+    public class PlaneBtn : MonoBehaviour
+    {
+        [SerializeField]
+        private ARPlaneManager arPlaneManager;
+
+        public void TogglePalne()
+        {
+            if(arPlaneManager.isActiveAndEnabled)
+            {
+                arPlaneManager.SetTrackablesActive(false);
+                arPlaneManager.enabled = false;
+            }
+            else
+            {
+                arPlaneManager.enabled = true;
+                arPlaneManager.SetTrackablesActive(true);
+            }
+        }
+    }
+}
